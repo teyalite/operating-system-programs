994533eaf7e92b722bd18bf7836349832458ae11
= <66052642+teyaligth@users.noreply.github.com>
2022-04-07 17:03:57 +0300

    added gcc log parser

---
+++ Gcc log parser/error.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    ;printf("%lf", -809);
+    int v_ double v_
+    ;double v_6d6f924a;
+    ;float v_fb8ffb76;
+    ;float v_d5771aa7;
+    float v_ double v_ double v_ char v_
+    double v_ int v_
+    ;printf("%lf %f", -612, -40);
+    double v_ int v_]
+    ;printf("%lf %f %f %s %s", -812, -853, 904, -371, 148);
+}
